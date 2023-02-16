aea8ba5aeaf0c8aec33bd90287ed241a9165873b
Lyubomir <stamenovl99@gmail.com>
2023-02-16 17:48:29 +0200

    fixed service

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,5 +1,5 @@
 export const API_KEY: string = '9467dada6c562150e0606a619c9ba8ff';
-export const API_URL_PROD = 'https://be-watchit.herokuapp.com';
+export const API_URL_PROD = 'https://watch-api-6wx4.onrender.com';
 export const API_URL_DEV = 'http://localhost:3001';
 export const API_URL = API_URL_PROD;
 
