c13d3ef1179afa2befef6b004974344ca7c06059
Lyubomir <stamenovl99@gmail.com>
2023-03-26 09:34:41 +0300

    translations hot fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { BrowserRouter } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n/i18n';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
-import './i18n/i18n';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
