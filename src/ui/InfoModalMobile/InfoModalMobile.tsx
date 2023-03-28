938216f28520f68dd6e059a1ea3638d2688eaeb7
Lyubomir <stamenovl99@gmail.com>
2023-03-28 11:22:26 +0300

    new MobileModal for info

---
+++ src/ui/InfoModalMobile/InfoModalMobile.tsx
@@ -0,0 +1,76 @@
+import React, { useMemo } from 'react';
+import { AppBar, DialogContent, Drawer, IconButton, Toolbar, Typography } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { TrendingMovie, TrendingShow } from '../../types/types';
+import { getMoviePosterPath } from '../../utils/movieUtils';
+import CarouselGenres from '../../components/carouselGenres/CarouselGenres';
+import PlayButton from '../../components/PlayButton/PlayButton';
+import AddToListButton from '../../components/AddToListButton/AddToListButton';
+import { default as AddToListButtonUI } from '../AddToListButton/AddToListButton';
+import styles from './InfoModalMobile.module.css';
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+  movie: TrendingMovie | TrendingShow;
+}
+
+const InfoModalMobile: React.FC<Props> = ({ isOpen, onClose, movie }) => {
+  const isShow = (object: any): object is TrendingShow => 'name' in object;
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const { title, overview, genres, backdrop_path, id } = useMemo(() => {
+    let movieTitle = '';
+
+    if (isShow(movie)) {
+      movieTitle = movie.name;
+    } else {
+      movieTitle = movie.title;
+    }
+
+    return {
+      title: movieTitle,
+      overview: movie.overview,
+      genres: movie.genres,
+      backdrop_path: movie.backdrop_path,
+      id: movie.id,
+    };
+  }, [movie]);
+
+  return (
+    <Drawer
+      PaperProps={{
+        style: {
+          backgroundColor: '#2d2e2e',
+        },
+      }}
+      open={isOpen}
+    >
+      <AppBar sx={{ position: 'relative', backgroundColor: '#4f4f4f' }}>
+        <Toolbar>
+          <IconButton onClick={onClose} edge="start" color="inherit">
+            <ArrowBackIcon />
+          </IconButton>
+          <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+            {title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <DialogContent sx={{ padding: 0 }}>
+        <img className={styles.picture} src={getMoviePosterPath(backdrop_path)} />
+        <Typography className={styles.overview}>{overview}</Typography>
+        <div className={styles.actions}>
+          <CarouselGenres genres={genres} numberToShow={3} />
+        </div>
+        <div className={styles.actions}>
+          <PlayButton size={75} url={isShow(movie) ? `/shows/play/${id}` : `/movies/play/${id}`} />
+          <AddToListButton movieName={title} movieId={id} isMovie={!isShow}>
+            {(props) => <AddToListButtonUI size={80} {...props} />}
+          </AddToListButton>
+        </div>
+      </DialogContent>
+    </Drawer>
+  );
+};
+
+export default InfoModalMobile;
