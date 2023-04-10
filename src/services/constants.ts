c236a058bbc8577a88409d4259b5ec103b48680f
Lyubomir <stamenovl99@gmail.com>
2023-04-10 12:50:52 +0300

    changed api env

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,4 +1,4 @@
 export const API_KEY = '9467dada6c562150e0606a619c9ba8ff';
 export const API_URL_PROD = 'https://shiny-fox-snaps.cyclic.app';
 export const API_URL_DEV = 'http://localhost:3001';
-export const API_URL = API_URL_DEV;
+export const API_URL = API_URL_PROD;
