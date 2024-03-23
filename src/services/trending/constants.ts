0778100f83e8af4afbc688b2d202a27a12e4eb5a
Lyubomir Stamenov <lyubomir.stamenov@dss.bg>
2024-03-23 18:35:56 +0200

    integrated new trending api

--- src/services/trending/constants.ts
+++
@@ -1,4 +0,0 @@
-import { API_KEY } from '../constants';
-
-export const TRENDING_WEEKLY = (lang: string, page: number): string =>
-  `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=${lang}&page=${page}`;
--- src/store/features/trendingSlice/thunk.ts
+++ src/store/features/trendingSlice/thunk.ts
@@ -1,19 +1,10 @@
 import { ActionReducerMapBuilder, createAction, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import i18n from '../../../i18n/i18n';
-import MovieService from '../../../services/movies/MovieService';
-import ShowService from '../../../services/shows/ShowService';
 import TrendingService from '../../../services/trending/TrendingService';
 import { TrendingState } from './trendingSlice';
 
-const movieService: MovieService = new MovieService(axios, i18n);
-const showService: ShowService = new ShowService(axios, i18n);
-const trendingService: TrendingService = new TrendingService(
-  axios,
-  i18n,
-  movieService,
-  showService,
-);
+const trendingService: TrendingService = new TrendingService(axios, i18n);
 
 export const fetchTrending = createAsyncThunk('trending/fetchTrending', async (page: number) => {
   const trending = await trendingService.fetchTrending(page);
